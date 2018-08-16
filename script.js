58e70bd2670ed9e1d8468057fea3a32af4c82cc2
Unknown <jhobeck376@west-mec.org>
2018-08-16 15:28:38 -0700

    Script Update 1
    Began scaffolding the file out. Created Event listeners for the load and for the submit events.

--- script.js
+++ script.js
@@ -7,3 +7,29 @@
     Filename: script.js
 */
 
+"use strict";
+
+// global variables
+var formValidity = true;
+
+// function to validate form
+function validateForm() {
+    alert("success");
+}
+
+// function to create event listeners
+function createEventListeners() {
+    var form = document.getElementsByTagName("form")[0];
+    if (form.addEventListener) {
+        form.addEventListener("submit", validateForm, false);
+    } else if (form.attachEvent) {
+        form.attachEvent("onsubmit", validateForm);
+    }
+}
+
+// page load event listeners
+if (window.addEventListener) {
+    window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", createEventListeners);
+}
